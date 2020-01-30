55e6055d4d5c7f34c683c56961bc113f3aa5dbed
Rena <info@tinycircuits.com>
2020-01-29 19:25:38 -0500

    zip zoop zap

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tinycircuits-bma250", 
-    version="0.0.1",
+    version="0.0.2",
     author="TinyCircuits",
     author_email="info@tinycircuits.com",
     description="Wireling Accelerometer Sensor Python library",
